b348d0aaa6f556ee2d9488fc20c8e63ee81f8e17
Alireza Keshavarz <ashalogic@outlook.com>
2019-09-06 05:00:25 +0300

    ver 0.8.4

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Fingerprint from "./Routes/Fingerprint";
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import Content from "./Routes/Content";
 
+let deferredPrompt; // Allows to show the install prompt
+let setupButton;
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -20,6 +23,40 @@ export default class App extends React.Component {
     // this.handleInputChange = this.handleInputChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.usernametextInput = React.createRef();
+
+    window.addEventListener("beforeinstallprompt", e => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      console.log("beforeinstallprompt fired");
+      if (setupButton == undefined) {
+        setupButton = document.getElementById("setup_button");
+      }
+      // Show the setup button
+      setupButton.style.display = "inline";
+      setupButton.disabled = false;
+    });
+    window.addEventListener("appinstalled", evt => {
+      console.log("appinstalled fired", evt);
+    });
+  }
+
+  installApp() {
+    // Show the prompt
+    deferredPrompt.prompt();
+    setupButton.disabled = true;
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("PWA setup accepted");
+        // hide our user interface that shows our A2HS button
+        setupButton.style.display = "none";
+      } else {
+        console.log("PWA setup rejected");
+      }
+      deferredPrompt = null;
+    });
   }
 
   handleInputChange(event) {
@@ -205,6 +242,10 @@ export default class App extends React.Component {
                 </ul>
               </div>
             </div>
+            <button id="setup_button" onClick={this.installApp}>
+              Installer
+            </button>
+
             <Route
               path="/"
               exact
