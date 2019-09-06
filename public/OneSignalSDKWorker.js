28be492f7a0c0193b3f353fefa9916deab57ecc5
Alireza Keshavarz <ashalogic@outlook.com>
2019-09-07 01:07:50 +0300

    Update

---
+++ public/OneSignalSDKWorker.js
@@ -0,0 +1,2 @@
+importScripts('./serviceWorker');
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.register();
 
 var OneSignal = window.OneSignal || [];
 OneSignal.push(function() {
@@ -18,4 +17,5 @@ OneSignal.push(function() {
     // appId: "9f9b8f25-9546-4bba-b536-c334d910e09d"
   });
 });
-// serviceWorker.register();
+
+serviceWorker.register();
